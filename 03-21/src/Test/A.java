8f310b44e7d2817d7998c0200a85f5e747fc98cc
주경민 <132@naver.com>
2022-10-14 00:35:52 +0900

    3. 03/21
    A B Test - add

---
+++ 03-21/src/Test/A.java
@@ -0,0 +1,13 @@
+package Test;
+
+class A {
+	private int result1;
+	public int add(int x,int y) {
+		result1 = x + y;
+		return result1;
+	}
+	public int sub(int x,int y) {
+		result1 = x - y;
+		return  result1;
+	}
+}
---
+++ 03-21/src/Test/B.java
@@ -0,0 +1,13 @@
+package Test;
+
+class B extends A {
+	private int result2;
+	public int multi(int x,int y) {
+		result2 = x*y;
+		return result2;
+	}
+	public int divide(int x,int y) {
+		result2= x/y;
+		return result2;
+	}
+}

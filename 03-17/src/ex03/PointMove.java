93e79e46f81f5f5d426ac3e8b653868887478230
주경민 <132@naver.com>
2022-10-14 00:32:11 +0900

    2. 03/17
    add point

---
+++ 03-17/src/ex03/PointMove.java
@@ -0,0 +1,10 @@
+package ex03;
+
+public class PointMove {
+	public static void main(String[] args) {
+		Point point1 = new Point();
+		point1.move(10,10);
+		System.out.print("x = "+ point1.xPosition + " y = "+ point1.yPosition);
+	}
+
+}
